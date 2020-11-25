02a772f5224b2934ed80450c65d60a48767a47b2
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2020-11-25 22:55:49 +0100

    Refactor; format and style improvements

--- mkProvenanceInfo.sh
+++ mkProvenanceInfo.sh
@@ -9,11 +9,12 @@
 # @title Write provenance information to config file
 # @description Prepare Phase: write info about competition and components used to file
 
+DIR=$(dirname "$0")
 VERIFIER=$1
-COMPETITIONNAME=`scripts/parseInitConfig.py --get-comp benchmark-defs/category-structure.yml`
-YEAR=`scripts/parseInitConfig.py --get-year benchmark-defs/category-structure.yml`
-TARGETSERVER=`echo ${COMPETITIONNAME} | tr [:upper:] [:lower:]`
-ARCHIVE="`pwd`/archives/${YEAR}/${VERIFIER}.zip"
+COMPETITIONNAME=$($DIR/parseInitConfig.py --get-comp $DIR/../benchmark-defs/category-structure.yml)
+YEAR=$($DIR/parseInitConfig.py --get-year $DIR/../benchmark-defs/category-structure.yml)
+TARGETSERVER=$(echo $COMPETITIONNAME | tr [:upper:] [:lower:])
+ARCHIVE="$DIR/../archives/$YEAR/$VERIFIER.zip"
 GIT_REPOS="archives sv-benchmarks benchexec scripts"
 
 if [ -z $VERIFIER ]; then
@@ -24,16 +25,16 @@ fi
 echo ""
 echo "Provenance information:"
 echo "Benchmark executed"
-echo "for ${COMPETITIONNAME} ${YEAR}, https://${TARGETSERVER}.sosy-lab.org/${YEAR}/"
-echo "by ${USER}@${HOSTNAME}"
+echo "for $COMPETITIONNAME $YEAR, https://$TARGETSERVER.sosy-lab.org/$YEAR/"
+echo "by $USER@$HOSTNAME"
+echo "on `date -Iminutes`"
 echo "based on the components"
 for repo in $GIT_REPOS; do
   (
-  cd "$repo"
+  cd "$DIR/../$repo"
   echo "`git remote get-url origin`  `git describe --long --always`"
   )
 done
 
-echo "Archive: ${VERIFIER}.zip  SHA1: "`shasum ${ARCHIVE} | sed "s/\(.\{10\}\).*/\1/"`"..."
-echo "on `date -Iminutes`"
+echo "Archive: $VERIFIER.zip  DATE: "$(date -Iminutes --date=@$(stat --format=%Y $ARCHIVE))"  SHA1: "`shasum $ARCHIVE | sed "s/\(.\{10\}\).*/\1/"`"..."
 echo ""
