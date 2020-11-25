aa356ca15ec892930c789d6b6358ded789edd343
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2020-11-25 22:14:37 +0100

    Print provenance info to stdout instead of into file.

--- mkProvenanceInfo.sh
+++ mkProvenanceInfo.sh
@@ -11,18 +11,15 @@
 COMPETITIONNAME=`scripts/parseInitConfig.py --get-comp benchmark-defs/category-structure.yml`;
 YEAR=`scripts/parseInitConfig.py --get-year benchmark-defs/category-structure.yml`;
 TARGETSERVER=`echo ${COMPETITIONNAME} | tr [:upper:] [:lower:]`
-export PROVENANCEFILE="`pwd`/provenance.txt";
-rm -f "$PROVENANCEFILE"
-touch "$PROVENANCEFILE"
-echo "" >> "$PROVENANCEFILE"
-echo "Provenance information:" >> "$PROVENANCEFILE"
-echo "Benchmark executed" >> "$PROVENANCEFILE"
-echo "for ${COMPETITIONNAME} ${YEAR}, https://${TARGETSERVER}.sosy-lab.org/${YEAR}/" >> "$PROVENANCEFILE"
-echo "by ${USER}@${HOSTNAME}" >> "$PROVENANCEFILE"
-echo "based on the components" >> "$PROVENANCEFILE"
+echo ""
+echo "Provenance information:"
+echo "Benchmark executed"
+echo "for ${COMPETITIONNAME} ${YEAR}, https://${TARGETSERVER}.sosy-lab.org/${YEAR}/"
+echo "by ${USER}@${HOSTNAME}"
+echo "based on the components"
 for repo in "$@"; do
   (
   cd "$repo"
-  echo "`git remote get-url origin`  `git describe --long --always`" >> "$PROVENANCEFILE"
+  echo "`git remote get-url origin`  `git describe --long --always`"
   )
 done
