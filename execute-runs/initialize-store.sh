db4aa047e145ff8c7cfa6928c0bf4c7ab5f90ed4
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2020-11-30 15:27:49 +0100

    Add scripts to process results competition

---
+++ execute-runs/initialize-store.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This file is part of the competition environment.
+#
+# SPDX-FileCopyrightText: 2011-2020 Dirk Beyer <https://www.sosy-lab.org>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# This script gets three parameters: log directory, witness name, witness glob suffix
+LOG_DIR=$1
+WITNESSTARGET=$2;
+WITNESSGLOBSUFFIX=$3;
+ROOT_DIR=$(realpath $(dirname "$0")/../..)
+SCRIPTS_DIR=$(dirname "$0")
+PYTHONPATH="$ROOTDIR/benchexec"
+HASHES_BASENAME="fileHashes.json"
+
+if [[ "$LOG_DIR" == "" || "$WITNESSTARGET" == "" || "$WITNESSGLOBSUFFIX" == "" ]]; then
+  echo "Usage: $0 <log directory> <witness name> <witness glob suffix>"
+  exit 1
+fi
+
+# Create hashes map for programs
+$SCRIPTS_DIR/create-hashes.py \
+  -o "${LOG_DIR%.files}.$HASHES_BASENAME" \
+  --root-dir "$ROOT_DIR" \
+  "$ROOT_DIR/sv-benchmarks/c"
+
+# Make sure that names of witnesses are always the same
+$SCRIPTS_DIR/create-uniform-witness-structure.py \
+  --copy-to-files-dir "$WITNESSTARGET" \
+  --glob "$WITNESSGLOBSUFFIX" \
+  "$LOG_DIR"
+
+# Create hashes map for witnesses/test-suites
+$SCRIPTS_DIR/create-hashes.py \
+  -o "${LOG_DIR%.files}.$HASHES_BASENAME" \
+  --root-dir "$ROOT_DIR" \
+  "$LOG_DIR"
+
---
+++ execute-runs/utils.py
@@ -0,0 +1,41 @@
+import fnmatch
+from math import floor, log10
+import os
+import zipfile
+
+JSON_INDENT = 4
+
+_BLACKLIST = (
+    "__CLOUD__created_files.txt",
+    "RunResult-*.zip",
+    "*.log.data",
+    "*.log.stdError",
+    "fileHashes.json",
+)
+"""List of files that are not hashed. May include wildcard '*' and brackets '[', ']'"""
+
+
+def is_on_blacklist(filename):
+    return any(fnmatch.fnmatch(os.path.basename(filename), b) for b in _BLACKLIST)
+
+
+def get_file_number_in_zip(zipped_file) -> int:
+    """Return the list of files in this zip"""
+    try:
+        with zipfile.ZipFile(zipped_file) as inp_zip:
+            return len(
+                [
+                    x
+                    for x in inp_zip.namelist()
+                    if x.endswith(".xml") and not x.endswith("metadata.xml")
+                ]
+            )
+    except FileNotFoundError as e:
+        # print('Error: Zip file does not exist.')
+        return 0
+
+
+def round_to_sig_numbers(x: float, n: int) -> float:
+    if x == 0:
+        return 0
+    return round(x, -int(floor(log10(abs(x)))) + (n - 1))
