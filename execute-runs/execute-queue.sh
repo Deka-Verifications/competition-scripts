cd75ea07025d2d3659cccd2f1b0bbda88e1d514e
Dirk Beyer <Dirk.Beyer@sosy-lab.org>
2021-10-11 17:29:17 +0200

    Move queue scripts from private repository to public repository (and rename)

---
+++ execute-runs/execute-queue.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set +e
+
+while true; do
+  # Update job queue.
+  scripts/execute-runs/update-queue.sh
+
+  # Process next job.
+  JOB=$(ls -t queue/ | grep -v "\(^README.md$\|\.wait$\|\.running$\|\.finished$\)" | tail -1)
+  if [[ "$JOB" == "" ]]; then
+    echo ""
+    echo "No jobs available. Build witness store if necessary."
+    date -Iseconds
+    sleep 600
+    continue
+  fi
+  echo ""
+  echo "Processing job: $JOB"
+  mv "queue/$JOB" "queue/$JOB.running"
+  sleep 10
+  contrib/mkRunVerify.sh "$JOB" |& tee -a "./results-logs/$JOB.log"
+  mv -f "queue/$JOB.running" "queue/$JOB.finished"
+done
+
+echo "All jobs finished."
+
---
+++ execute-runs/update-queue.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "----------------------------------------------"
+echo "Pulling new archives."
+pushd archives || exit
+git pull --rebase
+popd || exit
+
+echo ""
+echo "Add new jobs to the queue."
+YEAR=$(scripts/parseInitConfig.py benchmark-defs/category-structure.yml --get-year)
+for VERIFIER in archives/"$YEAR"/*.zip; do
+  JOB=$(basename "${VERIFIER%.zip}")
+  if [[ "$JOB" =~ "val_" ]]; then
+    continue
+  fi
+  echo ""
+  echo "Considering $JOB"
+  RUNS=$(find results-verified/ -maxdepth 1 -name "$JOB.*.logfiles.zip")
+  if [[ $RUNS != "" ]]; then
+    # Look up the most recent run result.
+    RESULT=$(ls -t $RUNS | head -1)
+    RESULT_TIME=${RESULT##*$JOB.}
+    RESULT_TIME=${RESULT_TIME%.logfiles.zip}
+    ARCHIVE_TIME=$(date "+%Y-%m-%d_%H-%M-%S" --reference="$VERIFIER")
+    echo "Time stamp of run-result: $RESULT_TIME"
+    echo "Time stamp of archive:    $ARCHIVE_TIME"
+    if [[ "$ARCHIVE_TIME" < "$RESULT_TIME" ]]; then
+      echo "Job was already processed."
+      continue
+    fi
+  fi
+  # Add job only if it was not already considered before.
+  if [[ -e "queue/$JOB"  ||  -e "queue/$JOB.wait"  ||  -e "queue/$JOB.running"  ||  -e "queue/$JOB.finished" ]] ; then
+    echo "Not scheduled $JOB"
+    continue
+  fi
+  echo "Scheduling $JOB"
+  touch --reference="$VERIFIER" "queue/$JOB"
+done
