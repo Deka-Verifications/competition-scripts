c63164f8dcf15060878fbe162923ad9983ae0753
Dirk Beyer <dirk.beyer@ifi.lmu.de>
2021-11-07 13:19:51 +0100

    Refactor: Remove unnecessary semicolons

--- execute-runs/mkInstall.sh
+++ execute-runs/mkInstall.sh
@@ -9,29 +9,29 @@
 # @title Install Tool Archive
 # @description Unzips and checks the structure of tool archive.
 
-TOOL=$1;
-TOOL_DIR=$2;
-YEAR=$(scripts/parseInitConfig.py --get-year benchmark-defs/category-structure.yml);
-ARCHIVE="$(pwd)/archives/${YEAR}/${TOOL}.zip";
+TOOL=$1
+TOOL_DIR=$2
+YEAR=$(scripts/parseInitConfig.py --get-year benchmark-defs/category-structure.yml)
+ARCHIVE="$(pwd)/archives/${YEAR}/${TOOL}.zip"
 
 if [[ -z "$TOOL" || -z "$TOOL_DIR" ]]; then
   echo "Usage: $0 <tool> <install directory>"
-  exit 1;
+  exit 1
 fi
 
 # Unzip
-echo "Installing $ARCHIVE ...";
+echo "Installing $ARCHIVE ..."
 cd "$TOOL_DIR" || exit
 unzip "$ARCHIVE"
 # Check structure
 if [[ $(find . -mindepth 1 -maxdepth 1 | wc -l) == 1 ]]; then
-  echo "Info: One folder found in archive.";
-  DIR="$(find . -mindepth 1 -maxdepth 1)";
-  mv "${DIR}" "${DIR}__COMP";
+  echo "Info: One folder found in archive."
+  DIR="$(find . -mindepth 1 -maxdepth 1)"
+  mv "${DIR}" "${DIR}__COMP"
   mv "${DIR}__COMP"/* .
   rmdir "${DIR}__COMP"
 else
-  echo "Error: Archive does not contain exactly one folder.";
-  exit 1;
+  echo "Error: Archive does not contain exactly one folder."
+  exit 1
 fi
 
