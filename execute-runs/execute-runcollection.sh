74e1bd17d16514a7ea1198e1d1018b8fe2b843e1
Dirk Beyer <dirk.beyer@ifi.lmu.de>
2021-12-04 15:59:29 +0100

    Refactor script `execute-runcollection.sh`: Change paths of argument directories and files such that they exist at the call site

--- execute-runs/execute-runcollection.sh
+++ execute-runs/execute-runcollection.sh
@@ -8,7 +8,7 @@
 
 # This script gets the following parameters:
 #   benchmarking script (I use `benchmark.py` here, to execute the benchmark of VerifierCloud, others can pass benchexec here)
-#   tool (matching name of benchmark definition XML file)
+#   tool archive (archive to run the benchmark with`)
 #   benchmark definition (XML file)
 #   witness name
 #   witness glob suffix
@@ -16,7 +16,7 @@
 #   parameters for benchmarking script (optional)
 
 BENCHEXEC_SCRIPT=$1
-TOOL=$2
+TOOL_ARCHIVE=$2
 BENCHMARK_DEFINITION_FILE=$3
 WITNESS_TARGET=$4
 WITNESS_GLOB_SUFFIX=$5
@@ -26,22 +26,45 @@ BENCHEXEC_OPTIONS=$7
 SCRIPTS_DIR=$(realpath "$(dirname "$0")")
 ROOT_DIR=$(realpath "$SCRIPTS_DIR/../..")
 
-if [[ "$BENCHEXEC_SCRIPT" == "" || "$TOOL" == "" || "$BENCHMARK_DEFINITION_FILE" == "" ||
+if [[ "$BENCHEXEC_SCRIPT" == "" || "$TOOL_ARCHIVE" == "" || "$BENCHMARK_DEFINITION_FILE" == "" ||
       "$WITNESS_TARGET" == "" || "$WITNESS_GLOB_SUFFIX" == "" || "$OUTPUT_DIR" == "" ]]; then
   echo "Usage: $0 <command> <tool> <benchmark definition> <witness name> <witness glob suffix> <output directory>"
   exit 1
 fi
 
-if [[ ! -e "$ROOT_DIR/benchmark-defs/$BENCHMARK_DEFINITION_FILE" ]]; then
+if [[ -e "$TOOL_ARCHIVE" ]]; then
+  TOOL="$(basename "${TOOL_ARCHIVE%.zip}")"
+else
+  echo "Tool archive $TOOL_ARCHIVE not found."
+  exit 1
+fi
+
+TOOL_DIR=$(mktemp --directory --tmpdir="./bin/" "$TOOL-XXXXXXXXXX")
+
+if [[ -e "$BENCHEXEC_SCRIPT" ]]; then
+  BENCHEXEC_SCRIPT="$(realpath --relative-to="$TOOL_DIR" "$BENCHEXEC_SCRIPT")"
+else
+  echo "Benchmark script $BENCHEXEC_SCRIPT not found."
+  exit 1
+fi
+
+if [[ -e "$BENCHMARK_DEFINITION_FILE" ]]; then
+  BENCHMARK_DEFINITION_FILE="$(realpath --relative-to="$TOOL_DIR" "$BENCHMARK_DEFINITION_FILE")"
+else
   echo "Benchmark definition $BENCHMARK_DEFINITION_FILE not found."
   exit 1
 fi
 
-echo "$TOOL with benchmark definition $BENCHMARK_DEFINITION_FILE waits 10 seconds first.";
+if [[ -e "$OUTPUT_DIR" ]]; then
+  OUTPUT_DIR="$(realpath --relative-to="$TOOL_DIR" "$OUTPUT_DIR")"
+else
+  echo "Output directory $OUTPUT_DIR not found."
+  exit 1
+fi
+
+echo "$TOOL with benchmark definition $(basename $BENCHMARK_DEFINITION_FILE) waits 10 seconds first.";
 sleep 10;
 
-
-TOOL_DIR=$(mktemp --directory --tmpdir="./bin/" "$TOOL-XXXXXXXXXX")
 
 echo ""
 echo "  Installing $TOOL in $TOOL_DIR"
@@ -57,14 +80,14 @@ if [[ ! -e $OUTPUT_DIR ]]; then
 fi
 TMP_FILE=$(mktemp --suffix=-provenance.txt)
 "$SCRIPTS_DIR/mkProvenanceInfo.sh" "$TOOL" > "$TMP_FILE"
-$BENCHEXEC_SCRIPT $BENCHEXEC_OPTIONS "../../benchmark-defs/$BENCHMARK_DEFINITION_FILE" -o "$OUTPUT_DIR" --description-file "$TMP_FILE"
+$BENCHEXEC_SCRIPT $BENCHEXEC_OPTIONS "$BENCHMARK_DEFINITION_FILE" -o "$OUTPUT_DIR" --description-file "$TMP_FILE"
 rm "$TMP_FILE"
 #rm -rf "$TOOL_DIR"
 
 echo ""
 echo "  Initialize store for $TOOL"
 cd "$OUTPUT_DIR" || exit
-RESULT_DIR=$(find . -maxdepth 1 -type f -name "${BENCHMARK_DEFINITION_FILE%.xml}.????-??-??_??-??-??.logfiles.zip" | sort --reverse | sed -e "s#^\./##" -e "s/\.logfiles\.zip$/.files/" | head -1)
+RESULT_DIR=$(find . -maxdepth 1 -type f -name "$(basename "${BENCHMARK_DEFINITION_FILE%.xml}").????-??-??_??-??-??.logfiles.zip" | sort --reverse | sed -e "s#^\./##" -e "s/\.logfiles\.zip$/.files/" | head -1)
 if [[ "$RESULT_DIR" == "" ]]; then
   echo "    No results (logs) found."
 else
