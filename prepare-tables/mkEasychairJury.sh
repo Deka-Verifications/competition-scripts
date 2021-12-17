aa174abdd6c8925c5826471d506d316bb49676a7
Dirk Beyer <dirk.beyer@ifi.lmu.de>
2021-12-17 11:33:46 +0100

    Make script more configurable

--- prepare-tables/mkEasychairJury.sh
+++ prepare-tables/mkEasychairJury.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source $(dirname "$0")/../configure.sh
+
 yq -r '.verifiers [] | if ."jury-member".name != "Hors Concours" then ."jury-member".name else empty end' benchmark-defs/category-structure.yml \
   | while IFS=$'\n' read MEMBERNAME; do
-  grep "$MEMBERNAME" ~/.competition-address-book.txt
+  grep "$MEMBERNAME" "$ADDRESS_BOOK"
 done | sort -u
 
